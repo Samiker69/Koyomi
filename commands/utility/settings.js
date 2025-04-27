cdcd51685d6709485da2e82f7317312dd6d10116
VinTeRuS <vinterus@gmail.com>
2025-04-28 00:12:18 +0300

    Фикс: changeSettings, добаление динамических рум (не работает, нужен фикс)

--- commands/utility/settings.js
+++ commands/utility/settings.js
@@ -10,71 +10,104 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('settings')
 		.setDescription('Изменить настройки бота')
-            .addSubcommand(subcommand => 
-                subcommand.setName('welcomechannel').setDescription('Канал для уведомления о новых участниках')
-                    .addChannelOption(option => option.setName('channel').setDescription('Канал').setRequired(true))).setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
-            .addSubcommand(subcommand => 
-                subcommand.setName('invite-logger-channel').setDescription('Канал для логироавния ссылок приглашений')
-                        .addChannelOption(option => option.setName('channel').setDescription('Канал').setRequired(true))).setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
-            .addSubcommand(subcommand => 
-                subcommand.setName('allowlogging').setDescription('Отправлять сообщения об использованной ссылке?')
-                    .addBooleanOption(option => option.setName('bool').setDescription('включить/выключить').setRequired(true))).setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
-            .addSubcommand(subcommand => 
-                subcommand.setName('allowmembersaddlogging').setDescription('Отправлять сообщения о новых/ушедших участниках')
-                    .addBooleanOption(option => option.setName('bool').setDescription('включить/выключить').setRequired(true))).setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
+        .addSubcommand(subcommand => 
+            subcommand.setName('welcomechannel').setDescription('Канал для уведомления о новых участниках')
+                .addChannelOption(option => option.setName('channel').setDescription('Канал').setRequired(true)))
+        .addSubcommand(subcommand => 
+            subcommand.setName('invite-logger-channel').setDescription('Канал для логирования ссылок приглашений')
+                .addChannelOption(option => option.setName('channel').setDescription('Канал').setRequired(true)))
+        .addSubcommand(subcommand => 
+            subcommand.setName('allowlogging').setDescription('Отправлять сообщения об использованной ссылке?')
+                .addBooleanOption(option => option.setName('bool').setDescription('включить/выключить').setRequired(true)))
+        .addSubcommand(subcommand => 
+            subcommand.setName('allowmembersaddlogging').setDescription('Отправлять сообщения о новых/ушедших участниках')
+                .addBooleanOption(option => option.setName('bool').setDescription('включить/выключить').setRequired(true)))
+        .addSubcommand(subcommand =>
+            subcommand.setName('set-voice-category').setDescription('Установить категорию для новых войс-каналов')
+                .addChannelOption(option => option.setName('category').setDescription('Категория').setRequired(true)))
+        .addSubcommand(subcommand =>
+            subcommand.setName('set-main-voice').setDescription('Установить основной голосовой канал для создания комнат')
+                .addChannelOption(option => option.setName('channel').setDescription('Основной войс').setRequired(true)))
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
 
 	async execute(interaction) {
         const channel = interaction.options.getChannel('channel');
+        const category = interaction.options.getChannel('category');
         const bool = interaction.options.getBoolean('bool');
 
         switch (interaction.options.getSubcommand()) {
             case "welcomechannel":
                 try {
                     ChangeSettings('newMemberChannelId', channel.id);
-                    await interaction.reply(`Теперь ${channel} выбран как канал для отслеживания участников`)
+                    await interaction.reply(`Теперь ${channel} выбран как канал для отслеживания участников`);
                 } catch (error) {
                     console.error(error);
-                    await interaction.reply('Не удалось изменить параметр.')
+                    await interaction.reply('Не удалось изменить параметр.');
                 }
                 break;
 
             case 'invite-logger-channel':
                 try {
                     ChangeSettings('inviteLoggerChannel', channel.id);
-                    await interaction.reply(`Теперь ${channel} выбран как канал для отслеживания приглашений`)
+                    await interaction.reply(`Теперь ${channel} выбран как канал для отслеживания приглашений`);
                 } catch (error) {
                     console.error(error);
-                    await interaction.reply('Не удалось изменить параметр.')
+                    await interaction.reply('Не удалось изменить параметр.');
                 }
                 break;
 
             case 'allowlogging':
                 try {
-                    if (data.allowInviteLogging = bool) return await interaction.reply('Этот параметр уже установлен на ' + bool)
+                    if (data.allowInviteLogging === bool) return await interaction.reply('Этот параметр уже установлен на ' + bool);
                     let answerLog;
                     ChangeSettings('allowInviteLogging', bool);
-                    if (bool) {answerLog = 'Теперь бот будет уведомлять об использованной ссылке-приглашения'} else {answerLog = 'Теперь бот не будет уведомлять об использованной ссылке-приглашения'}
+                    if (bool) {answerLog = 'Теперь бот будет уведомлять об использованной ссылке-приглашения';} 
+                    else {answerLog = 'Теперь бот не будет уведомлять об использованной ссылке-приглашения';}
                     await interaction.reply(answerLog);
                 } catch (error) {
                     console.error(error);
-                    await interaction.reply('Не удалось изменить параметр.')
+                    await interaction.reply('Не удалось изменить параметр.');
                 }
                 break;
 
             case 'allowmembersaddlogging':
                 try {
-                    if (data.allowLogingMembersAdd = bool) return await interaction.reply('Этот параметр уже установлен на ' + bool)
+                    if (data.allowLogingMembersAdd === bool) return await interaction.reply('Этот параметр уже установлен на ' + bool);
                     let answerMemberLog;
                     ChangeSettings('allowLogingMembersAdd', bool);
-                    if (bool) {answerMemberLog = 'Теперь бот будет уведомлять об новых/ушедших участниках'} else {answerMemberLog = 'Теперь бот не будет уведомлять об новых/ушедших участниках'}
+                    if (bool) {answerMemberLog = 'Теперь бот будет уведомлять о новых/ушедших участниках';} 
+                    else {answerMemberLog = 'Теперь бот не будет уведомлять о новых/ушедших участниках';}
                     await interaction.reply(answerMemberLog);
                 } catch (error) {
                     console.error(error);
-                    await interaction.reply('Не удалось изменить параметр.')
+                    await interaction.reply('Не удалось изменить параметр.');
                 }
                 break;
+
+            case 'set-voice-category':
+                try {
+                    if (category.type !== 4) return await interaction.reply('Выберите именно категорию!');
+                    ChangeSettings('voiceCategoryId', category.id);
+                    await interaction.reply(`Теперь ${category} выбрана как категория для новых войс-каналов`);
+                } catch (error) {
+                    console.error(error);
+                    await interaction.reply('Не удалось изменить параметр.');
+                }
+                break;
+
+            case 'set-main-voice':
+                try {
+                    if (channel.type !== 2) return await interaction.reply('Выберите именно голосовой канал!');
+                    ChangeSettings('mainVoiceChannelId', channel.id);
+                    await interaction.reply(`Теперь ${channel} выбран как основной голосовой канал для создания комнат`);
+                } catch (error) {
+                    console.error(error);
+                    await interaction.reply('Не удалось изменить параметр.');
+                }
+                break;
+
             default:
                 break;
         }
     }
-}
+};
