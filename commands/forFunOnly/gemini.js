1411808775aab712f36854edc7a9010c80fd23f2
Samiker69 <Samiker69@github.com>
2025-05-09 18:24:08 +0300

    Обновлён gemini.js - поддерживает настройку конфига.   - кроме настроек безопасности - может ответить несколькими сообщениями, если исходное сообщение очень длинное

--- commands/forFunOnly/gemini.js
+++ commands/forFunOnly/gemini.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder, MessageFlags } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, MessageFlags, ActionRowBuilder, ButtonBuilder } = require('discord.js');
 const { forFunOnly } = require('../../locales/descriptions/forFunOnly')
 const { privateAccess, bot_log_channel } = require('../../config.json');
 
@@ -42,6 +42,48 @@ module.exports = {
         sub.setName("settings")
         .setDescription("Показывает ваши текущие настройки AI")
     )
+    .addSubcommand(sub =>
+        sub.setName('edit')
+        .setDescription('Сменить настройки ai')
+        .addStringOption(opt =>
+            opt.setName('model')
+            .setDescription('Модель для генерации')
+        )
+        .addStringOption(opt =>
+            opt.setName('system_instructions')
+            .setDescription('Системные инструкции.')
+        )
+        .addIntegerOption(opt =>
+            opt.setName('max_output_tokens')
+            .setDescription('Максимум токенов, которыми AI ответит. Если максимум меньше, чем ответила AI, то ответ обрежется!')
+            .setMaxValue(8196)
+            .setMinValue(1)
+        )
+        .addNumberOption(opt =>
+            opt.setName('temperature')
+            .setDescription('Регулирует случайность(креатичность ответа). 0.1 - предсказуемые, 1-2 - очень хаотичные')
+            .setMaxValue(2)
+            .setMinValue(0.1)
+        )
+        .addNumberOption(opt =>
+            opt.setName('top_p')
+            .setDescription('Вероятность использования токена. 0.0 - ожидаемый токен, 1 - использует все токены')
+            .setMaxValue(1)
+            .setMinValue(0.0)
+        )
+        .addIntegerOption(opt =>
+            opt.setName('top_k')
+            .setDescription('"Словарный запас" при генерации токена. 1-5 - маленький, 50+ - разнообразный')
+            .setMaxValue(100)
+            .setMinValue(1)
+        )
+        .addIntegerOption(opt =>
+            opt.setName('history_limit')
+            .setDescription('Лимит сохранения истории. 0 - без сохранения')
+            .setMaxValue(500)
+            .setMinValue(0)
+        )
+    )
 ,
     async execute(interaction) {
         if (!privateAccess.includes(interaction.user.id)) return await interaction.reply({ content: `Вы не можете использовать эту команду`, flags: MessageFlags.Ephemeral});
@@ -79,7 +121,18 @@ module.exports = {
                             ]
                         }
                     })
-                    await interaction.editReply(response.text)                    
+                    const reply = response.text || response.candidates[0].content || `Кажется... ai не ответила. Причина: [${response.candidates[0].finishReason}](<https://google.com/search?q=ai+returned+a+${response.candidates[0].finishReason}+response.+what+to+do>)`;
+                
+                    if (String(reply).length >= 1990) {
+                        const splited = splitStringByLength(String(reply), 1990)
+                        for (part of splited) {
+                            await interaction.editReply(part);
+                            await new Promise(resolve => setTimeout(resolve, 300)); 
+                        }
+                    }
+
+                    await interaction.editReply(reply);
+                    console.log(response, response.candidates[0].content)
                 } catch (error) {
                     await interaction.editReply(`ъ\n\`\`\`txt\n${error}\`\`\``)  
                     const errorEmbed = new EmbedBuilder()
@@ -91,7 +144,7 @@ module.exports = {
             )
             .setTimestamp(new Date())
             console.error(error);
-            const logChannel = await interaction.client.channels.fetch(bot_log_channel)
+            const logChannel = await interaction.client.channels.fetch(bot_log_channel);
             await logChannel.send({ embeds: [errorEmbed] });
                 }
                 break;
@@ -147,15 +200,32 @@ module.exports = {
                 .setAuthor({ iconURL: interaction.user.displayAvatarURL({extension: "png"}), name: interaction.user.displayName })
                 .setColor("Random")
                 .setTitle('Ваши настройки gemini')
-                .setDescription(`Системные инструкции: ${config.system_instructions || "Пусто"}\n\nНастройки безопасности:\n\`\`\`json\n${JSON.stringify(SS, null, 2)}\`\`\``)
+                .setDescription(`Системные инструкции(system_instructions): ${config.system_instructions || "Пусто"}\n\nНастройки безопасности:\n\`\`\`json\n${JSON.stringify(SS, null, 2)}\`\`\`\n`)
                 .setFields(
-                    { name: "Модель", value: config.model, inline: true },
-                    { name: "Максимум токенов на выходе", value: `${config.max_output_tokens}`, inline: true },
-                    { name: "Температура ответов", value: `${config.temperature}`, inline: true },
-                    { name: "topK", value: `${config.top_k}`, inline: true },
-                    { name: "topP", value: `${config.top_p}`, inline: true }
+                    { name: "Модель(model)", value: config.model, inline: true },
+                    { name: "Максимум токенов на ответ(max_output_tokens)", value: `${config.max_output_tokens}`, inline: true },
+                    { name: "Температура ответов(temperature)", value: `${config.temperature}`, inline: true },
+                    { name: "top_k", value: `${config.top_k}`, inline: true },
+                    { name: "top_p", value: `${config.top_p}`, inline: true },
+                    { name: "Лимит сохранения истории(history_limit)", value: `${config.history_limit}`, inline: true }
                 )
                 await interaction.reply({ embeds: [embed], flags: MessageFlags.Ephemeral })
+                break;
+            }
+            case "edit": {
+                if (!config) return await interaction.reply({ content: "Кажется, вас ещё нет в базе данных.", flags: MessageFlags.Ephemeral });
+                let changes = {
+                    model: interaction.options.getString('model') || config.model,
+                    system_instructions: interaction.options.getString('system_instructions') || config.system_instructions,
+                    max_output_tokens: interaction.options.getInteger('max_output_tokens') || config.max_output_tokens,
+                    temperature: interaction.options.getNumber('temperature') || config.temperature,
+                    top_p: interaction.options.getNumber('top_p') || config.top_p,
+                    top_k: interaction.options.getInteger('top_k') || config.top_k,
+                    history_limit: interaction.options.getString('history_limit') || config.history_limit
+                }
+                const result = db.updateUserConfig(interaction.user.id, changes);
+                const reply = result.changes > 0 ? "Настройки были обновленны." : "Настройки не изменились"
+                await interaction.reply({ content: reply, flags: MessageFlags.Ephemeral })
                 break;
             }
         
@@ -165,3 +235,14 @@ module.exports = {
         }
     }  
 }
+
+function splitStringByLength(str, maxLength) {
+    const parts = [];
+    if (!str) {
+      return parts;
+    }
+    for (let i = 0; i < str.length; i += maxLength) {
+      parts.push(str.substring(i, i + maxLength));
+    }
+    return parts;
+  }
