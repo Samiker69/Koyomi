93442d7ec518ef4ceaff7ee3cce2f90839b81f29
Samiker69 <Samiker69@github.com>
2025-05-10 22:22:03 +0300

    Исправлен gemini.js - корректо должен отправлять сообщения по частям - может быть использован как app в лс и группах

--- commands/forFunOnly/gemini.js
+++ commands/forFunOnly/gemini.js
@@ -19,7 +19,7 @@ module.exports = {
             .setMaxLength(2000)
             .setRequired(true)
         )
-    )
+    ).setContexts(0,1,2)
     .addSubcommand(sub =>
         sub.setName('add-user')
         .setDescription('Добавляет пользователя в бд, позволяя ему пользоваться командой /ai')
@@ -121,20 +121,20 @@ module.exports = {
                             ]
                         }
                     })
-                    await interaction.channel.sendTyping()
+                    if (interaction.guild) await interaction.channel.sendTyping()
+                   
                     const reply = response.text || response.candidates[0].content || `Кажется... ai не ответила. Причина: [${response.candidates[0].finishReason}](<https://google.com/search?q=ai+returned+a+${response.candidates[0].finishReason}+response.+what+to+do>)`;
                 
                     if (String(reply).length >= 1990) {
                         const splited = splitStringByLength(String(reply), 1990);
                         await interaction.editReply(part);
-                        for (part of splited) {
+                        for (const part of splited) {
                             await interaction.followUp(part);
                             await new Promise(resolve => setTimeout(resolve, 300)); 
                         }
                     }
 
                     await interaction.editReply(reply);
-                    console.log(response, response.candidates[0].content)
                 } catch (error) {
                     await interaction.editReply(`ъ\n\`\`\`txt\n${error}\`\`\``)  
                     const errorEmbed = new EmbedBuilder()
