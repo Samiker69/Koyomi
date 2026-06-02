9460bdff836a0e0bf72ad2600451d189e72d6e24
Yarpopcat08 <109970976+Yarpopcat08@users.noreply.github.com>
2026-06-02 14:03:13 +0300

    Добавил чек на кик-пермы в айкьютест

--- services/MojoTestService.js
+++ services/MojoTestService.js
@@ -21,6 +21,7 @@ class MojoTestService {
         const guild = interaction.guild;
         const targetUser = targetMember.user;
         const lang = interaction.guildLocale || 'ru';
+        if (!targetMember.permissions.has(PermissionFlagsBits.KickMembers)) return;
 
         // Если тест уже идёт для этого юзера
         if (this.activeTests.has(targetUser.id)) {
