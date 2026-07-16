de1932ffce29462aed7f0ebca5a1f6a1c5209808
Samiker <168203446+Samiker69@users.noreply.github.com>
2026-07-16 23:30:03 +0300

    clean: Удалены неиспользуемые/бесполезные части кода

--- bot.js
+++ bot.js
@@ -7,8 +7,6 @@ const EventHandler = require('./core/handlers/eventHandler');
 const ServiceHandler = require('./core/handlers/serviceHandler');
 const CommandDeployer = require('./core/utils/deployCommands');
 const KeyRotator = require('./lib/KeyRotator/KeyRotator');
-const BotIPCServer = require('./bot-ipc-server');
-const panelServer = require('./panel/panelServer')
 const DatabaseService = require('./services/DatabaseService');
 
 function setupApiKeys(client) {
@@ -16,26 +14,9 @@ function setupApiKeys(client) {
     client.keyManager = kr;
 }
 
-function setupIPCServerAndGracefulShutdown(client) {
-    const wsPort = process.env.WS_PORT || 8765;
-    const ipcServer = new BotIPCServer(client, wsPort);
-    ipcServer.start();
-    client.ipcServer = ipcServer;
-    const shutdown = () => {
-        console.log('\n[INFO] Получен сигнал завершения работы...');
-        if (client.ipcServer) client.ipcServer.stop();
-        client.services.forEach(service => service.stop());
-        client.destroy();
-        process.exit(0);
-    };
-    process.on('SIGINT', shutdown);
-    process.on('SIGTERM', shutdown);
-}
-
 async function bootstrap() {
     await DatabaseService.init();
     const client = new BaseBot();
-    setupApiKeys(client);
     const commandsPath = path.join(__dirname, 'commands');
     const eventsPath = path.join(__dirname, 'events');
     const servicesPath = path.join(__dirname, 'services');
@@ -46,8 +27,6 @@ async function bootstrap() {
     }
     const deployer = new CommandDeployer();
     await deployer.deploy(client.commands);
-    setupIPCServerAndGracefulShutdown(client);
-    panelServer.startPanel();
     await client.login(process.env.token || process.env.TOKEN);
 }
 
